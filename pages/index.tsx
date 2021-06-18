6a88da25c76a111e4d23ece10de3da037584d4c1
David Wolf <david.mwbp@gmail.com>
2021-06-18 21:45:01 +0200

    Add ShowOverview to index, add useShows hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,41 @@
+import ShowOverview, {
+	ShowOverviewProps
+} from "components/showOverview/ShowOverview";
+import { showsFetcher } from "lib/hooks/useShows";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import React from "react";
 
-function Home(): JSX.Element {
+function Home({ shows, showId }: ShowOverviewProps): JSX.Element {
 	return (
 		<>
 			<Head>
 				<title>Vice - Shows</title>
 			</Head>
-			<div>Render Shows here!</div>
+			<ShowOverview shows={shows} showId={showId} />
 		</>
 	);
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+	const { id } = query;
+
+	try {
+		const shows = await showsFetcher("http://localhost:3001/shows");
+
+		return {
+			props: {
+				shows,
+				showId: id ?? null
+			}
+		};
+	} catch (error) {
+		return {
+			props: {
+				shows: null
+			}
+		};
+	}
+};
 
 export default Home;
