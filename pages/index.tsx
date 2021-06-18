83058d17da8e8782d7f15e512be05a1b115c39f0
David Wolf <david.mwbp@gmail.com>
2021-06-18 03:24:57 +0200

    Add index page

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+function Home(): JSX.Element {
+	return (
+		<>
+			<Head>
+				<title>Vice - Shows</title>
+			</Head>
+			<div>Render Shows here!</div>
+		</>
+	);
+}
+
+export default Home;
